bc4c0e50601c846a2151c263cefc784d2f4dbfbb
HannachiHassen <hannachihassen07@gmail.com>
2023-03-01 13:58:14 -0500

    Update postTest method

--- src/test/java/com/RestAssured/PostRequest.java
+++ src/test/java/com/RestAssured/PostRequest.java
@@ -4,25 +4,37 @@ import static io.restassured.RestAssured.given;
 
 import org.testng.annotations.Test;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public final class PostRequest {
 
 	public PostRequest() {	}
 	
-	// 1. Passing json body as String --> Not recommanded	
+	// 1. Passing json body as String --> Not recommended	
 	@Test
 	public void postTest() {
-		Response response=given().body("{\r\n"
+		String rebBody="{\r\n"
 				+ "				\"id\":\"15\",\r\n"
 				+ "				\"first_name\":\"amuthan\",\r\n"
 				+ "				\"last_name\":\"sakthivel\",\r\n"
 				+ "				\"email\":\"abcd@gmail.com\"\r\n"
-				+ "		        }")
+				+ "		        }";
+		
+		Response response=given()
+				.header("Content-Type","application/json")
+				.header("Content-Type",ContentType.JSON)
+				.body(rebBody)
 				.log()
 				.all()
 				.post("http://localhost:3000/emmployees");
 		response.prettyPrint();
 		System.out.println(response.getStatusCode());
-	}	
+	}
+	
+	// 2. Passing json body as String --> Recommended
+	public void postTest2() {
+		
+	}
+	
 }
