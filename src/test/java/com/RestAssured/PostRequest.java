4dac9affb3dce24037e2397b122a963adde5fc5c
HannachiHassen <hannachihassen07@gmail.com>
2023-03-01 14:33:24 -0500

    new method to [200~

--- src/test/java/com/RestAssured/PostRequest.java
+++ src/test/java/com/RestAssured/PostRequest.java
@@ -3,6 +3,9 @@ package com.RestAssured;
 import static io.restassured.RestAssured.given;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.testng.annotations.Test;
 
@@ -11,11 +14,18 @@ import io.restassured.response.Response;
 
 public final class PostRequest {
 
-	public PostRequest() {	}
+	public PostRequest() {	
+		
+	}
 	
-	// 1. Passing JSON body as String --> Not recommended	
-	// Easy to copy and past --> can be used to quickly check the behavior
-	// Not recommended for larger JSON or dynamic JSON
+	/**
+	 * 
+	 * Passing JSON body as String --> Not recommended	
+	   1. Easy to copy and past --> can be used to quickly check the behavior
+	   2. Not recommended for larger JSON or dynamic JSON
+	 * 
+	 */
+	
 	@Test
 	public void postTest() {
 		String rebBody="{\r\n"
@@ -32,18 +42,50 @@ public final class PostRequest {
 				.log()
 				.all()
 				.post("http://localhost:3000/emmployees");
+		
 		response.prettyPrint();
 		System.out.println(response.getStatusCode());
 	}
 	
-	// 2. Passing from external file --> Recommended
-	// You cannot get the request form the file and print it on the console
+	/**
+	 * 
+	 * Pass it from external file
+	   1. You cannot get the request form the file and print it on the console
+	   2. Use this only for Static data
+	 * 
+	 */
+
 	@Test
 	public void postTest2() {
 		Response response=given()
 				.header("Content-Type",ContentType.JSON)
 				.body(new File(System.getProperty("user.dir") + "/test.json"))
 				.post("http://localhost:3000/emmployees");
+		
+		response.prettyPrint();
+		System.out.println(response.getStatusCode());
+	}
+	
+	/**
+	 * 
+	 * Read request body from an external file and convert to string
+	   1. Logs the request body into the console
+	   2. Change few parameters in the request
+	 * @throws IOException
+	 */
+		
+	@Test
+	public void postTest3() throws IOException {
+		byte[] bytes=Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "/test.json"));
+		String reqBody=new String(bytes);
+		
+		Response response=given()
+				.header("Content-Type",ContentType.JSON)
+				.log()
+				.all()
+				.body(reqBody)
+				.post("http://localhost:3000/emmployees");
+		
 		response.prettyPrint();
 		System.out.println(response.getStatusCode());
 	}
