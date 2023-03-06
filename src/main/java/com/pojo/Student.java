9863a056f02ca6e0190c48f3b38a15712704473a
HannachiHassen <hannachihassen07@gmail.com>
2023-03-06 15:52:38 -0500

    modify Student and add static Inner class

--- src/main/java/com/pojo/Student.java
+++ src/main/java/com/pojo/Student.java
@@ -17,4 +17,46 @@ public final class Student {
 	private String firstnatme;
 	private String lastname;
 	private String email;
+	
+	public static String address; //static field
+	
+	/**
+	 * Static Inner Class
+	 * @author HASSEN
+	 *
+	 */
+	public static class StudentBuilder1 {
+		private int id;
+		private String firstnatme;
+		private String lastname;
+		private String email;
+				
+		public static StudentBuilder1 builder() {
+			return new StudentBuilder1();
+		}		
+
+		public StudentBuilder1 setId(int id) {
+			this.id = id;
+			return this;
+		}
+
+		public StudentBuilder1 setFirstnatme(String firstnatme) {
+			this.firstnatme = firstnatme;
+			return this;
+		}
+
+		public StudentBuilder1 setLastname(String lastname) {
+			this.lastname = lastname;
+			return this;
+		}
+
+		public StudentBuilder1 setEmail(String email) {
+			this.email = email;
+			return this;
+		}		
+		
+		public Student build() {
+			return new Student(this.id, this.firstnatme, this.lastname, this.email);
+		}		
+	}
 }
