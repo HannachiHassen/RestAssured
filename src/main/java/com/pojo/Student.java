f6a938eb43dfa8e3009aa5435c05b73e0fa8620c
HannachiHassen <hannachihassen07@gmail.com>
2023-03-06 15:38:04 -0500

    modify Student class, add new BuilderDesingPatternTest class

--- src/main/java/com/pojo/Student.java
+++ src/main/java/com/pojo/Student.java
@@ -2,14 +2,19 @@ package com.pojo;
 
 
 import lombok.AllArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.*;
 
 @Getter
 @AllArgsConstructor
+@ToString
+@JsonInclude (value=JsonInclude.Include.NON_EMPTY, content=Include.NON_NULL)
 public final class Student {
 	private int id;
 	private String firstnatme;
 	private String lastname;
 	private String email;
-
 }
