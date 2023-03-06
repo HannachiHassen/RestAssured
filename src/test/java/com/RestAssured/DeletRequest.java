db85ddeb1fc0d49c5296da244045ae9b9e186d86
HannachiHassen <hannachihassen07@gmail.com>
2023-03-06 10:47:49 -0500

    Add new DeletRequest class

---
+++ src/test/java/com/RestAssured/DeletRequest.java
@@ -0,0 +1,32 @@
+package com.restassured;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public final class DeletRequest {
+
+	public DeletRequest() {
+		// TODO Auto-generated constructor stub
+	}
+	@Test
+	public void deleteEmployee() {
+	Response response = given()
+			.param("id", 3)
+			.log()
+			.all()
+			.delete("http://localhost:3000/emmployees/{id}");
+	
+	System.out.println(response.getStatusCode());
+	System.out.println(response.getTime());
+	System.out.println(response.getTimeIn(TimeUnit.SECONDS));
+	System.out.println(response.getContentType());		
+	System.out.println(response.getHeader("Content-Type"));
+	}
+
+}
